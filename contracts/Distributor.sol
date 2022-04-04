de95e0974a71ad6c9e853804a26cc032ae260690
bitdeep <19378023+bitdeep@users.noreply.github.com>
2022-04-04 20:04:04 -0300

    distritor: swap and distribute fees

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "./hermesswap/interfaces/IHermesRouter02.sol";
 import "./hermesswap/interfaces/IHermesFactory.sol";
 import "./hermesswap/interfaces/IHermesPair.sol";
@@ -12,22 +12,33 @@ contract Distributor {
     mapping(address => address[]) public tokensWithPathForXHRMS;
     mapping(address => address[]) public tokensWithPathForSHRMS;
     address[] tokens;
+    address public wone;
+    address public ust;
+    address public HRMS;
     address public treasury;
-    address public immutable router;
-    address public immutable xHRMSAddress;
-    address public immutable sHRMSAddress;
+    address public router;
+    address public xHRMSAddress;
+    address public sHRMSAddress;
     IHermesRouter02 public routerCtx;
     IHermesFactory public factoryCtx;
 
-    constructor(address _router, address _treasury, address _xHRMSAddress, address _sHRMSAddress) {
+    constructor(
+        address _router, address _treasury,
+        address _xHRMSAddress, address _sHRMSAddress,
+        address _ust, address _HRMS) {
         router = _router;
         treasury = _treasury;
         xHRMSAddress = _xHRMSAddress;
         sHRMSAddress = _sHRMSAddress;
 
+        ust = _ust;
+        HRMS = _HRMS;
+
         routerCtx = IHermesRouter02(_router);
         factoryCtx = IHermesFactory(routerCtx.factory());
         factoryCtx.allPairsLength();
+
+        wone = routerCtx.WONE();
     }
 
     function addNewToken(address _token, address[] memory _xHRMSPath, address[] memory _sHRMSPath) external {
@@ -39,67 +50,120 @@ contract Distributor {
         tokensWithPathForSHRMS[_token] = _sHRMSPath;
         tokens.push(_token);
     }
-    function breakLp() external {
+    function run() public {
+        breakLp();
+        convertAll();
+        splitAndSend();
+    }
+    function breakLp() public {
         uint length = factoryCtx.allPairsLength();
+        //console.log('length', length);
         for (uint i = 0; i < length; i ++) {
             IHermesPair pair = IHermesPair(factoryCtx.allPairs(i));
-            uint balanceOfLp = pair.balanceOf( address(this) );
-            if( balanceOfLp == 0 ) continue;
-            console.log('pair', balanceOfLp, address(pair));
+            uint balanceOfLp = pair.balanceOf(address(this));
+            //console.log('pair', balanceOfLp, address(pair));
+            if (balanceOfLp == 0) continue;
             IERC20Hermes token0 = IERC20Hermes(pair.token0());
             IERC20Hermes token1 = IERC20Hermes(pair.token1());
             pair.approve(address(routerCtx), balanceOfLp);
             routerCtx.removeLiquidity(pair.token1(), pair.token0(), balanceOfLp,
-                0, 0, address(this), block.timestamp+60);
+                0, 0, address(this), block.timestamp + 60);
             uint balance0 = token0.balanceOf(address(this));
             uint balance1 = token1.balanceOf(address(this));
             (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair.getReserves();
-            console.log(' token0', address(token0), balance0);
-            console.log('  reserve0', reserve0);
-            console.log(' token1', address(token1), balance1);
-            console.log('  reserve1', reserve1);
+//            console.log(' token0', address(token0), balance0);
+//            console.log('  reserve0', reserve0);
+//            console.log(' token1', address(token1), balance1);
+//            console.log('  reserve1', reserve1);
+        }
+    }
+
+    function convertAll() public {
+        uint length = factoryCtx.allPairsLength();
+        for (uint i = 0; i < length; i ++) {
+            IHermesPair pair = IHermesPair(factoryCtx.allPairs(i));
+            IERC20Hermes token0 = IERC20Hermes(pair.token0());
+            IERC20Hermes token1 = IERC20Hermes(pair.token1());
+            uint balance0 = token0.balanceOf(address(this));
+            uint balance1 = token1.balanceOf(address(this));
+            (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair.getReserves();
+//            console.log('----------------------------------------------------------------------');
+//            console.log(' l/token0', address(token0), balance0);
+//            console.log('  l/reserve0', reserve0);
+//            console.log(' l/token1', address(token1), balance1);
+//            console.log('  l/reserve1', reserve1);
             convert(token0);
             convert(token1);
         }
     }
 
     function convert(IERC20Hermes token) public {
+
         address tokenAddress = address(token);
-        if( ! tokenHashPath[ tokenAddress ] ){
+        if (tokenAddress == wone) {
+            //console.log('  // is wone token', token.symbol());
+            // no need to apply conversion for fee token
+            return;
+        }
+        if (!tokenHashPath[tokenAddress]) {
             // token has not path
+            //console.log('  // token has not path', token.symbol());
             return;
         }
 
         uint balanceOfToken = token.balanceOf(address(this));
-        if (balanceOfToken < 1 * 10**(token.decimals())){
+        if (balanceOfToken < 1 * 10 ** (token.decimals())) {
             // insufficient balance
+            //console.log('  // insufficient balance', token.symbol());
             return;
         }
 
 
-        // transfer 50% to treasury
-        uint treasuryAmount = balanceOfToken / 2;
-        token.transfer(treasury, treasuryAmount);
-        // swap 25% to xHRMS
-        uint xHRMSAmount = (balanceOfToken - treasuryAmount) / 2;
+        //
+        IERC20Hermes(tokensWithPathForXHRMS[tokenAddress][0])
+        .approve(address(routerCtx), balanceOfToken);
+
         routerCtx.swapExactTokensForTokens(
-            xHRMSAmount,
+            balanceOfToken,
             0,
             tokensWithPathForXHRMS[tokenAddress],
-            xHRMSAddress,
+            address(this),
             block.timestamp + 10000
         );
+        //console.log(' OK, swaped', tokenAddress);
+    }
 
-        // swap 25% of sHRMS
-        uint sHRMSAmount = balanceOfToken - treasuryAmount - xHRMSAmount;
-        routerCtx.swapExactTokensForTokens(
-            sHRMSAmount,
-            0,
-            tokensWithPathForXHRMS[tokenAddress],
-            sHRMSAddress,
-            block.timestamp + 10000
+    function splitAndSend() public {
+        IERC20Hermes token = IERC20Hermes(wone);
+
+        uint treasureBalance = token.balanceOf(address(this))/2;
+        uint xHRMSBalance = token.balanceOf(address(this))/4;
+        uint sHRMSBalance = token.balanceOf(address(this))/4;
+        //console.log('balances', treasureBalance, xHRMSBalance, sHRMSBalance);
+
+        token.transfer(treasury, treasureBalance);
+
+        address[] memory path1 = new address[](2);
+        path1[0] = wone;
+        path1[1] = ust;
+        IERC20Hermes(wone).approve(address(routerCtx), sHRMSBalance);
+        uint256[] memory amountsShrms = routerCtx.swapExactTokensForTokens(
+            sHRMSBalance, 0, path1, sHRMSAddress, block.timestamp + 10000
         );
+//        for( uint i = 0 ; i < amountsShrms.length ; i ++ ){
+//            console.log('- amountsShrms', amountsShrms[i]);
+//        }
 
+        address[] memory path2 = new address[](2);
+        path2[0] = wone;
+        path2[1] = HRMS;
+        IERC20Hermes(wone).approve(address(routerCtx), sHRMSBalance);
+        uint256[] memory amountsXhrms = routerCtx.swapExactTokensForTokens(
+            sHRMSBalance, 0, path2, xHRMSAddress, block.timestamp + 10000
+        );
+//        for( uint i = 0 ; i < amountsXhrms.length ; i ++ ){
+//            console.log('- amountsXhrms', amountsXhrms[i]);
+//        }
 
     }
 
--- test/Distributor.test.ts
+++ test/Distributor.test.ts
@@ -1,118 +1,144 @@
-import { ethers, network } from "hardhat"
-import { expect } from "chai"
-import { ADDRESS_ZERO, advanceTimeAndBlock, advanceBlockTo, latest, duration, increase } from "./utilities"
+import {ethers, network} from "hardhat"
+import {expect} from "chai"
+import {ADDRESS_ZERO, advanceTimeAndBlock, advanceBlockTo, latest, duration, increase} from "./utilities"
 
 
 function toWei(v: string): string {
-  return ethers.utils.parseUnits(v, 18).toString();
+    return ethers.utils.parseUnits(v, 18).toString();
 }
 
 function fromWei(v: string): string {
-  return ethers.utils.formatUnits(v, 18).toString();
+    return ethers.utils.formatUnits(v, 18).toString();
 }
 
 function toGWei(v: string): string {
-  return ethers.utils.parseUnits(v, 9).toString();
+    return ethers.utils.parseUnits(v, 9).toString();
 }
 
 function fromGWei(v: string): string {
-  return ethers.utils.formatUnits(v, 9).toString();
+    return ethers.utils.formatUnits(v, 9).toString();
 }
 
 function now(x: number) {
-  let t = new Date().getTime() / 1000;
-  t += x;
-  return parseInt(t.toString());
+    let t = new Date().getTime() / 1000;
+    t += x;
+    return parseInt(t.toString());
 }
 
 
 describe("Distributor", function () {
-  before(async function () {
-    this.signers = await ethers.getSigners()
-    this.dev = this.signers[0]
-    this.treasure = this.signers[1]
-    this.xHRMSAddress = this.signers[2]
-    this.sHRMSAddress = this.signers[3]
+    before(async function () {
+        this.signers = await ethers.getSigners()
+        this.dev = this.signers[0]
+        this.treasure = this.signers[1]
+        this.xHRMSAddress = this.signers[2]
+        this.sHRMSAddress = this.signers[3]
 
-    this.Distributor = await ethers.getContractFactory("Distributor")
-    this.ERC20MockDecimals = await ethers.getContractFactory("ERC20MockDecimals")
-    this.HermesPair = await ethers.getContractFactory("HermesPair")
-    this.HermesFactory = await ethers.getContractFactory("HermesFactory")
-    this.HermesRouter02 = await ethers.getContractFactory("HermesRouter02")
-    this.WONE9Mock = await ethers.getContractFactory("WONE9Mock")
+        this.Distributor = await ethers.getContractFactory("Distributor")
+        this.ERC20MockDecimals = await ethers.getContractFactory("ERC20MockDecimals")
+        this.HermesPair = await ethers.getContractFactory("HermesPair")
+        this.HermesFactory = await ethers.getContractFactory("HermesFactory")
+        this.HermesRouter02 = await ethers.getContractFactory("HermesRouter02")
+        this.WONE9Mock = await ethers.getContractFactory("WONE9Mock")
 
-    this.wone = await this.WONE9Mock.deploy();
-    await this.wone.deployed()
+        this.wone = await this.WONE9Mock.deploy();
+        await this.wone.deployed()
 
-    this.factory = await this.HermesFactory.deploy(this.dev.address);
-    await this.factory.deployed()
-    this.router = await this.HermesRouter02.deploy(this.factory.address, this.wone.address);
-    await this.router.deployed()
+        this.factory = await this.HermesFactory.deploy(this.dev.address);
+        await this.factory.deployed()
+        this.router = await this.HermesRouter02.deploy(this.factory.address, this.wone.address);
+        await this.router.deployed()
 
-    const pairCodeHash = await this.factory.pairCodeHash();
-    // expect(pairCodeHash).to.be.eq('0x83bc7bb34b67d17e1f2fc8ad5d7a0f8a374a52474546c4ecbfb368acdc0ff2e1');
-    expect(pairCodeHash).to.be.eq('0x2b5c1f92763cc808d46f799ed176bc720efdf2600b6b6ba91296a61afd2aa00b');
+        const pairCodeHash = await this.factory.pairCodeHash();
+        // expect(pairCodeHash).to.be.eq('0x83bc7bb34b67d17e1f2fc8ad5d7a0f8a374a52474546c4ecbfb368acdc0ff2e1');
+        expect(pairCodeHash).to.be.eq('0x2b5c1f92763cc808d46f799ed176bc720efdf2600b6b6ba91296a61afd2aa00b');
 
-  })
-
-  beforeEach(async function () {
-    this.hermes = await this.ERC20MockDecimals.deploy(18)
-    await this.hermes.deployed()
-    await this.hermes.mint(this.dev.address, ethers.utils.parseUnits('9000000', 18).toString() );
-
-    this.dai = await this.ERC20MockDecimals.deploy(18)
-    await this.dai.deployed()
-    await this.dai.mint(this.dev.address, ethers.utils.parseUnits('9000000', 18).toString() );
-
-    this.main = await this.Distributor.deploy(this.router.address, this.treasure.address, this.xHRMSAddress.address, this.sHRMSAddress.address);
-    await this.main.deployed()
-    await this.factory.setFeeTo(this.main.address);
-
-    await this.hermes.approve(this.router.address, ethers.utils.parseUnits('9000000', 18).toString() );
-    await this.dai.approve(this.router.address, ethers.utils.parseUnits('9000000', 18).toString() );
-
-    this.router.addLiquidity(
-        this.hermes.address, this.dai.address,
-        ethers.utils.parseUnits('1000000', 18).toString(),
-        ethers.utils.parseUnits('1000000', 18).toString(),
-        '0', '0', this.dev.address, '9647704139')
-
-    const amount = ethers.utils.parseUnits('100000', 18).toString();
-    const path = [this.hermes.address, this.dai.address];
-    await this.router.swapExactTokensForTokensSupportingFeeOnTransferTokens(amount,'0',path,this.dev.address, '9647704139');
-
-    this.router.addLiquidity(
-        this.hermes.address, this.dai.address,
-        ethers.utils.parseUnits('1000000', 18).toString(),
-        ethers.utils.parseUnits('1000000', 18).toString(),
-        '0', '0', this.dev.address, '9647704139')
-
-    this.pair_hrms_dai_addr = await this.factory.getPair(this.hermes.address, this.dai.address);
-    console.log('this.pair_hrms_dai_addr', this.pair_hrms_dai_addr);
-
-    this.hrmsdai = this.HermesPair.attach(this.pair_hrms_dai_addr);
-
-    const hrmsdaiBalanceOfMain =  fromWei( (await this.hrmsdai.balanceOf(this.main.address)).toString() );
-    console.log('hrmsdaiBalanceOfMain', hrmsdaiBalanceOfMain);
-    await expect(hrmsdaiBalanceOfMain).to.be.eq("19.482227428699307951")
-
-    // const hrmsdaiBalanceOfDev = (await this.hrmsdai.balanceOf(this.dev.address)).toString();
-    // await expect(hrmsdaiBalanceOfDev).to.be.eq("0")
-    await this.main.breakLp();
-  })
-
-  it("check balances", async function () {
-    // const amount = ethers.utils.parseUnits('100000', 18).toString();
-    // const path = [this.hermes.address, this.dai.address];
-    // await this.router.swapExactTokensForTokensSupportingFeeOnTransferTokens(amount,'0',path,this.dev.address, '9647704139');
-  })
-
-
-
-  after(async function () {
-    await network.provider.request({
-      method: "hardhat_reset",
-      params: [],
     })
-  })
+
+    it("swap fees and check balances", async function () {
+        this.hermes = await this.ERC20MockDecimals.deploy(18)
+        await this.hermes.deployed()
+        await this.hermes.mint(this.dev.address, ethers.utils.parseUnits('9000000', 18).toString());
+
+        this.ust = await this.ERC20MockDecimals.deploy(18)
+        await this.ust.deployed()
+        await this.ust.mint(this.dev.address, ethers.utils.parseUnits('9000000', 18).toString());
+
+        this.dai = await this.ERC20MockDecimals.deploy(18)
+        await this.dai.deployed()
+        await this.dai.mint(this.dev.address, ethers.utils.parseUnits('9000000', 18).toString());
+
+        this.main = await this.Distributor.deploy(
+            this.router.address, this.treasure.address,
+            this.xHRMSAddress.address, this.sHRMSAddress.address,
+            this.ust.address, this.hermes.address);
+        await this.main.deployed()
+        await this.factory.setFeeTo(this.main.address);
+
+        await this.main.addNewToken(this.hermes.address,
+            [this.hermes.address, this.wone.address],
+            [this.hermes.address, this.wone.address]);
+
+        await this.hermes.approve(this.router.address, ethers.utils.parseUnits('9000000', 18).toString());
+        await this.dai.approve(this.router.address, ethers.utils.parseUnits('9000000', 18).toString());
+        await this.ust.approve(this.router.address, ethers.utils.parseUnits('9000000', 18).toString());
+
+        const amount = ethers.utils.parseUnits('1000000', 18).toString();
+        const amount1000 = ethers.utils.parseUnits('1000', 18).toString();
+        this.router.addLiquidity(
+            this.hermes.address, this.dai.address, amount, amount,
+            '0', '0', this.dev.address, '9647704139')
+        this.router.addLiquidity(
+            this.hermes.address, this.ust.address, amount, amount,
+            '0', '0', this.dev.address, '9647704139')
+
+        this.router.addLiquidityONE(
+            this.hermes.address,
+            amount, '0', '0', this.dev.address, '9647704139', {value: amount1000})
+
+        this.router.addLiquidityONE(
+            this.ust.address,
+            amount, '0', '0', this.dev.address, '9647704139', {value: amount1000})
+
+        const amount1 = ethers.utils.parseUnits('10000', 18).toString();
+        const path1 = [this.hermes.address, this.dai.address];
+        const path2 = [this.hermes.address, this.wone.address];
+        await this.router.swapExactTokensForTokensSupportingFeeOnTransferTokens(amount1, '0', path1, this.dev.address, '9647704139');
+        await this.router.swapExactTokensForTokensSupportingFeeOnTransferTokens(amount1, '0', path2, this.dev.address, '9647704139');
+
+        this.router.addLiquidity(
+            this.hermes.address, this.dai.address,
+            amount,
+            amount,
+            '0', '0', this.dev.address, '9647704139')
+
+        this.pair_hrms_dai_addr = await this.factory.getPair(this.hermes.address, this.dai.address);
+
+        // console.log('this.pair_hrms_dai_addr', this.pair_hrms_dai_addr);
+
+        this.hrmsdai = this.HermesPair.attach(this.pair_hrms_dai_addr);
+
+        const hrmsdaiBalanceOfMain = fromWei((await this.hrmsdai.balanceOf(this.main.address)).toString());
+        // console.log('hrmsdaiBalanceOfMain', hrmsdaiBalanceOfMain);
+        await expect(hrmsdaiBalanceOfMain).to.be.eq("2.121660991929858591")
+
+        await this.main.run();
+
+        const xHRMSBalance = (await this.hermes.balanceOf(this.xHRMSAddress.address)).toString();
+        await expect(xHRMSBalance).to.be.eq("532509624773258313")
+
+        const sHRMSBalance = (await this.ust.balanceOf(this.sHRMSAddress.address)).toString();
+        await expect(sHRMSBalance).to.be.eq("522030378254482673")
+
+        const treasureBalance = (await this.wone.balanceOf(this.treasure.address)).toString();
+        await expect(treasureBalance).to.be.eq("1047202910271497")
+
+    })
+
+    after(async function () {
+        await network.provider.request({
+            method: "hardhat_reset",
+            params: [],
+        })
+    })
 })
